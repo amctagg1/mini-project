fd9a40b7bbdf713ac542a5aff31d83675730149b
Abbey McTaggart <amctagg1@gmail.com>
2022-03-26 18:50:47 -0500

    comment re: pages in API

--- src/app/api_responses/paginginfo.ts
+++ src/app/api_responses/paginginfo.ts
@@ -1,6 +1,6 @@
 export class PagingInfo {
     count: number;
-    pages: number;
+    pages: number; // Since pages is already determined before the API response, I don't see a way to change 20 results per page to 5 results per page, to match the mockup
     next: string;
     prev: string;
 }
